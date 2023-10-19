14099306144fc8ec95660d73072329938162cbdf
centinna-l <jerryjoy99@gmail.com>
2023-10-19 13:40:24 -0400

    Swap 2 numbers

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Swapping 2 numbers
+
+int a = 15;
+int b = 100;
+
+// you have to swap 2 numbers 
+// a= 20, b=10
+
+// 1st method
+int temp;
+temp = a;
+a = b;
+b = temp;
+
+
+// Second Method
+// Ideal when we have positive numbers (integers). 
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+Console.WriteLine($"a is {a}");
+Console.WriteLine($"b is {b}");
+
+//Bubble Sort.
+//Bubble Sort is the simplest sorting algorithm that works
+//by repeatedly swapping the adjacent elements if they are in the wrong order.
+//This algorithm is not suitable for large data sets as its average 
+//and worst-case time complexity is quite high.
+int[] arr = { 5, 3, 1, 4, 2 };
+
+for (int i = 0; i < 20; i++)
+{
+    for (int j = 0; j < 30; j++)
+    {
+        // 10 * 10 = 100
+        // 10 * 30 = 300
+        // 20 * 30 = 600
+
+        // i is running upto n times
+        // j is running upto m times
+        // the total number of iterations will be n*m
+        // Time complexity in programming is denoted by O() (Big O)
+        // O(n*m) AVGERAGE CASE
+        // O(n*n) Worst case => O(n^2) 
+        // O(n^2) is the worst case in programming.
+    }
+}
+
+
+
