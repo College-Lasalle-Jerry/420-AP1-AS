88d82c82ac7a2cad150544f46706672700c67088
centinna-l <jerryjoy99@gmail.com>
2023-11-10 14:27:21 -0500

    Binary Search

--- Program.cs
+++ Program.cs
@@ -1,223 +1,68 @@
-﻿// 1. Write a method to find and return the second largest element in an integer array. 
-// Provide the method and an example. 
+﻿// Linear Search
 
-// Ex - [10, 13, -1, 2, 6, 7] => 10 is the second largest element 
 
-// static int getSecondLargestNumber(int[] array)
+// we have an array (int), and we need to find if a value is present or not.
+// 10,15,23,-1,2,4
+// find -1
+
+// static string search(int[] array, int search)
 // {
 
-//     // run the loop once. 
-//     // we take 2 variables
-//     // firstLargest
-//     // SecondLargest
-//     int firstLargest = int.MinValue;
-//     int SecondLargest = int.MinValue;
-//     // foreach loop
 //     foreach (int element in array)
 //     {
-//         if (element > firstLargest)
+//         if (element == search)
 //         {
-//             SecondLargest = firstLargest;
-//             firstLargest = element;
-//         }
-//         else if (element > SecondLargest && element != firstLargest)
-//         {
-//             SecondLargest = element;
+//             return "Element found!";
 //         }
 //     }
-//     return SecondLargest;
+//     return "Element not found";
 // }
 
-// // 2. Create a method that checks if a string contains only unique characters (no duplicates).  
-// // Ex – Hello -> duplicates present,
-// // Ex – bye -> no duplicates present 
-
-// // return type: bool
-// // input: string
-
-// static bool checkDuplicates(string str)
-// {
-//     // check duplicates
-//     for (int i = 0; i < str.Length; i++)
-//     {
-//         for (int j = 1; j < str.Length; j++)
-//         {
-//             if (str[i] == str[j])
-//             {
-//                 //hello
-//                 // h e, h l, h l, h 0
-//                 // e l, e l, e 0
-//                 // l l, => false
-//                 return false;
-//             }
-//         }
-//     }
-//     return true;
-// }
-
-// // find a better solution. (HW)
-
-// // 3. Write a method that calculates the sum of the squares of the first n natural numbers.  
-// // Ex – 10, 1+2+3+4+5+6+7+8+9+10 => 55 
-
-// // return : int
-// // input: int
-
-// static int getSumOfNaturalNumber(int n)
-// {
-//     int sum = 0;
-//     for (int i = 1; i <= n; i++)
-//     {
-//         //sum = sum + n;
-//         sum += n;
-//     }
-//     return sum;
-// }
-
-// // 4. Write a method that calculates the power of a number (x^n) without using the built-in Math.Pow function. (Use For /while Loop)
-// // Ex – x = 2, n = 2 => 2^2  => 4  
-
-// // return : int number, int power
-// // input: int
-
-// static int getPower(int number, int power)
-// {
-//     // if the number is negative;
-//     // -2^2 => -2 * -2 => 4
-//     // -2^3 => -2 * -2 * -2 =>-8
-//     if (number < 0)
-//     {
-//         power = 1 / power;
-//         number = -number;
-//     }
-//     int result = 1;
-//     for (int i = 1; i <= power; i++)
-//     {
-//         result = result * number;
-//     }
-//     return result;
-// }
-
-// //5. GCD
-// // Greatest Common Denominator
-// // return as int
-// // input : int a, int b
-
-// static int calculateGCD(int a, int b)
-// {
-//     // a/b, 3/0 => Infinity, 0/1 => undefined.
-//     while (b != 0)
-//     {
-//         int temp = b;
-//         b = a % b;
-//         a = temp;
-
-//         // 10/5
-//         // temp = 5
-//         // b = 10%5 => 0
-//         // a = 5
-//     }
-//     return a;
-// }
+// int[] array = { 10, 15, 23, -1, 2, 4 };
+// Console.WriteLine("Enter an element to search.");
+// int searchElement = int.Parse(Console.ReadLine());
+// string result = search(array, searchElement);
+// Console.WriteLine(result);
 
 
-// // Time Complexity
-// // 1. 
-
-// Random rnd = new Random();
-// int a = 0, b = 0;
-// int N = 10, M = 10;
-// for (int i = 0; i < N; i++)
-// {
-//     a = a + rnd.Next();
-// }
-// // 0 to N => O(N)
-// for (int j = 0; j < M; j++)
-// {
-//     b = b + rnd.Next();
-// }
-// // 0 to M => O(M)
+// Compare the middle element of the search space with the key. 
+// If the key is found at middle element, the process is terminated.
+// If the key is not found at middle element, choose which half will be used as the next search space.
+// If the key is smaller than the middle element, then the left side is used for next search.
+// If the key is larger than the middle element, then the right side is used for next search.
+// This process is continued until the key is found or the total search space is exhausted.
 
 
-// // time complexity => O(N+M) => O(N)
 
-
-// //2. 
-// a = 0;
-// for (int i = 0; i < N; i++)
-// {
-//     for (int j = N; j > i; j--)
-//     {
-//         a = a + i + j;
-//     }
-// }
-
-// Time Complexity => O(N) * O(N) => O(N^2)
-
-//3. 
-int i, j, k = 0;
-int n = 32;
-for (i = n / 2; i <= n; i++)
+static int BinarySearchFunction(int[] array, int searchElement)
 {
-    Console.WriteLine(i); // n/2
-    for (j = 2; j <= n; j = j * 2)
+    int left = 0, right = array.Length - 1;
+    // Loop
+
+    while (left <= right)
     {
-        k = k + n / 2;
-        Console.WriteLine("Value of J" + j); // logn
+        int middle = left + (right - left) / 2; // to avoid overflow.
+        // if middle element of array is equal to the searchElement.
+        if (array[middle] == searchElement)
+        {
+            return middle;
+        }
+        // if the se if greate than middle element.
+        if (searchElement > array[middle])
+        {
+            left = middle + 1;
+        }
+        else
+        {
+            right = middle - 1;
+        }
     }
-}
-// Time Complexity => O(n/2) * O(logn)=> O(nlogn)
 
-
-//4.
-int N = 20;
-int a = 0;
-i = N;
-while (i > 0)
-{
-    a += i;
-    i /= 2;
+    return -1; // if no element is found we return -1
 }
 
+int[] array = { -1, 2, 4, 10, 15, 23 };
 
-static int getFactorial(int number)
-{
-    int fact = 1;
-    for (int i = 1; i <= number; i++)
-    {
-        fact = fact * i;
-    }
-    return fact;
-}
+// i will terenay operators to print result;
 
-static bool checkStrongNumber(int number)
-{
-    int copy = number;
-    int sum = 0;
-    while (number > 0) // 2 times 
-    {
-        int digit = number % 10; // extract the last digit.
-        sum = sum + getFactorial(digit); // add the factorial to the sum.
-        // 1 and 2 times 
-        number = number / 10; // remove the last digit.
-    }
-    return copy == sum; // true other false
-}
-
-// while (12>0)
-// 2 => getFactorial(2) => 2 (Runs 2 times)
-// while (1>0)
-// 1 => getFactorial(1) => 1 (Runs once)
-// O(No of Digits * digit) 
-
-// if (copy != sum)
-// {
-//     return false;
-// }
-// return true;
-// return copy == sum ? true : false;
-
-
-// Methods
-// Time Complexity
-// Thursday 1 -4 PM Online.
+Console.WriteLine(BinarySearchFunction(array, 15) == -1 ? "Element not Found" : "Element Found!");
